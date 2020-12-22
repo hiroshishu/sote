62be036d09a21bb1015f3eae8c4dc52beb30e55a
oakatplatform <qiuatibm@163.com>
2020-12-22 20:14:37 +0800

    install

--- frontend/install.sh
+++ frontend/install.sh
@@ -8,3 +8,7 @@ npm run build:prod
 
 cp public/data/settingsm.json dist/data/settings.json
 tar -zcvf dist.tar.gz dist > /dev/null
+
+scp -i ~/3.pem dist.tar.gz linshi@13.70.3.247:/home/linshi
+
+#scp -i ~/Downloads/other-mongo.pem dist.tar.gz centos@18.166.235.68:/home/centos
